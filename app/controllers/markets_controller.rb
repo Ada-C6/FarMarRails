a19f2e2d038adacbb1fed177e2fadad5e562eecc
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-04 16:51:55 -0700

    finished initial Market index controller and view

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,14 @@
+class MarketsController < ApplicationController
+  def index
+    @markets = Market.all
+  end
+
+
+# TODO:
+# markets#create
+# markets#new
+# markets#edit
+# markets#show
+# markets#update
+
+end
