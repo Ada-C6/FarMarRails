0fde98e6811eadf755b37dd1f44820eddc104dfe
guineveresaenger <guineveresaenger@yahoo.com>
2016-10-04 14:21:52 -0700

    Nonworking seeds file that won't run from the command line

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# for markets:
+
+markets = []
+
+CSV.read('/../../seed_csvs/markets.csv').map do |line|
+  market_hash = {}
+  market_hash[:name] = line[1]
+  market_hash[:address] = line[2]
+  market_hash[:city] = line[3]
+  market_hash[:state] = line[4]
+  market_hash[:zip] = line[5]
+  markets << market_hash
+end
+
+markets.each do |market_hash|
+  Market.create(market_hash)
+end
+
+
+
+# CSV.read('/../../seed_csvs/markets.csv').map do |line|
+#   market_hash = {
+#     #id: line[0].to_i,
+#     name: line[1],
+#     address:line[2],
+#     city: line[3],
+#     county: line[4],
+#     state: line[5],
+#     zip: line[6]
+#   }
+#   Market.create(market_hash)
+# end
