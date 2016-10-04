dbaf9a7c48d07640c3e1fad5b3904f73f33a18c6
Yeni <yenicapote08@gmail.com>
2016-10-04 14:41:49 -0700

    Added empty methods to market, product, vendor, sale controllers.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,12 @@
 class ProductController < ApplicationController
+  def create
+
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
 end
--- app/controllers/sale_controller.rb
+++ app/controllers/sale_controller.rb
@@ -1,2 +1,12 @@
 class SaleController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
 end
