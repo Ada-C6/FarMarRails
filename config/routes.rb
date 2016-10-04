10c1b469b5eea253a4c0088941fa0661565ba6a7
heatherherrington <redherring@gmail.com>
2016-10-04 16:54:55 -0700

    Added text on pages regarding what info needs to go there

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/users/markets' => 'users#markets'
   get '/users/markets/:id' => 'users#show'
 
-  resources :markets
+  resources :markets, except: [:destroy]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
