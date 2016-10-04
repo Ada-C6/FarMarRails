bd1b2865aba212903e36bbe60fb94105d5b223cc
LaurenSky <ljherrer@gmail.com>
2016-10-04 16:46:46 -0700

    added page to view vendors and view products by vendor & added gems

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,29 @@
 class ProductsController < ApplicationController
+
+  def index
+    @all_products_by_vendor = Product.where(vendor_id: params[:vendor_id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def Update
+    @product = Product.find(params[:id])
+
+  if @product.update(task_params)
+    # saved successfully
+    redirect_to product_path
+  else
+    # did not save
+    render :edit
+  end
+  end
+
+  private
+
+  def task_params
+    params.require(:product).permit(:name)
+  end
+
 end
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,2 +1,13 @@
 class VendorsController < ApplicationController
+# As a vendor, I can create, edit, or delete my Products
+# As a vendor, I can create a Sale for one of my Products
+# As a vendor, I can see a list of my Sales
+# As a vendor, I can see a total of all of my Sales
+# As a vendor, I can see a list of my Sales for the current month
+# As a vendor, I can see a total of my Sales for the current month
+
+  def index
+    @all_vendors = Vendor.all
+  end
+
 end
