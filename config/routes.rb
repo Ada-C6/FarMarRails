55d81ee1e2ddb9cd0c1723bea529acf71f2a6bb0
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-04 16:08:43 -0700

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :markets, except: [:destroy]
+  resources :vendors do
+    resources :products, except: [:show]
+    resources :sales, only: [:create, :new, :index]
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
