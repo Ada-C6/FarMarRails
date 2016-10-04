d86797e0a3ca19b7b314ec6551c3bad29c3a310d
Jessica <jchung722@gmail.com>
2016-10-04 15:31:50 -0700

    added models

---
+++ db/migrate/20161004220345_create_vendors.rb
@@ -0,0 +1,12 @@
+class CreateVendors < ActiveRecord::Migration
+  def change
+    create_table :vendors do |t|
+      t.string :name
+      t.integer :no_employees
+      t.integer :market_id
+      t.references :market, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
