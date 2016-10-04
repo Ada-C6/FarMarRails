bdf02c38471d8b79fd67715e812dc738e3c123c3
yaelittle <yaelittle@gmail.com>
2016-10-04 16:07:44 -0700

    created all models

---
+++ db/migrate/20161004230453_create_vendors.rb
@@ -0,0 +1,12 @@
+class CreateVendors < ActiveRecord::Migration
+  def change
+    create_table :vendors do |t|
+      t.integer :id
+      t.string :name
+      t.integer :num_employees
+      t.integer :market_id
+
+      t.timestamps null: false
+    end
+  end
+end
