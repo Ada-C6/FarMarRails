f62e89161894104e3b60d1e4644d23f86a411dcb
Sarah Nahmias <nahmisa@gmail.com>
2016-10-04 16:58:22 -0700

    Allows a market to edit a market

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -11,11 +11,25 @@ class MarketsController < ApplicationController
     @market = Market.new
   end
 
+  def edit
+    @market = Market.find(params[:id])
+  end
+
   def create
     @market = Market.new(market_params)
 
     if @market.save
       redirect_to markets_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @market = Market.find(params[:id])
+
+    if @market.update(market_params)
+      redirect_to market_path
     else
       render :new
     end
