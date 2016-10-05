870816731be2a626e373035d2d1bb92c0b319248
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-05 14:50:36 -0700

    continued pairing to draft our initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,32 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  get '/home' => 'home#index'
+  resources :home, only: [:index] do
+      resources :markets, only: [:index, :show] do
+          resources :vendors, only: [:index]
+      end
+  end
+
+  resources :markets, except: [:destroy] do
+      resources :vendors, except: [:show]
+  end
+
+  resources :vendors, only: [:index, :show] do
+      resources :products, except: [:show] do
+          resources :sales, only: [:new]
+      end
+      resources :sales, only: [:index]
+  end
+
+  # get 'markets' => 'markets#index'
+  # get 'markets/new' => 'markets#new'
+  # post 'markets' => 'markets#create'
+  #
+  # get 'markets/:id' => 'markets#show', as: 'market'
+  #
+  # get 'markets/:id/edit' => 'markets#edit', as: 'markets_edit'
+  # patch 'markets/:id' => 'markets#update'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
