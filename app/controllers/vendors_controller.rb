80bd0dff580bc42472bdec3db60c7ce604bdbffa
heatherherrington <redherring@gmail.com>
2016-10-05 15:01:49 -0700

    Added list of products and sales to vendor pages

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,7 +1,11 @@
 class VendorsController < ApplicationController
 
   def index
-
+    @vendor = Vendor.all
   end
-  
+
+  def show
+    @vendor = Vendor.find(params[:id])
+  end
+
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,4 +2,5 @@ class Vendor < ActiveRecord::Base
   belongs_to :market
   has_many :products
   has_many :sales
+
 end
