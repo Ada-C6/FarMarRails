101a4b52c207892474016954ec8f9e73b90117b8
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-05 13:54:51 -0700

    updated seeds.rb to read data from Vendor, Product and Sale csv files

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,31 @@ CSV.read('./seed_csvs/markets.csv').each do |line|
     zip: line[6]
   }
   Market.create(csv_market)
+end
 
+CSV.read('./seed_csvs/vendors.csv').each do |line|
+  csv_vendor = {
+    name: line[1],
+    num_employees: line[2],
+    market_id: line[3]
+  }
+  Vendor.create(csv_vendor)
+end
+
+CSV.read('./seed_csvs/products.csv').each do |line|
+  csv_product = {
+    name: line[1],
+    vendor_id: line[2]
+  }
+  Product.create(csv_product)
+end
+
+CSV.read('./seed_csvs/sales.csv').each do |line|
+  csv_sale = {
+    amount: line[1],
+    purchase_time: line[2],
+    vendor_id: line[3],
+    product_id: line[4]
+  }
+  Sale.create(csv_sale)
 end
