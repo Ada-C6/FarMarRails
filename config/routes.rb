076bc6560148982390a4b7889d6860669ed5b252
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-05 14:30:35 -0700

    nested vendor and market routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :markets, only: [:index, :show]
+  # root 'user#index'
+
+  # resources :user, only: [:index] do
+    resources :markets, only: [:index, :show] do
+      resources :vendors
+    end
+  # end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
