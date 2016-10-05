6b571ee488c5c872eb337f01b4e2fae81ee87ad2
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-05 13:59:41 -0700

    added the ability to edit and save an existing market

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,6 +1,8 @@
 class MarketsController < ApplicationController
 
-  def index; end
+  def index
+      @markets = Market.all
+   end
 
   def all
     #put a list of all the markets here
@@ -12,4 +14,24 @@ class MarketsController < ApplicationController
     @vendors_list = @market.vendors
   end
 
+  def edit
+    @edit_market = Market.find(params[:id])
+  end
+
+  def update
+    @edit_market = Market.find(params[:id])
+    if @edit_market.update(market_params) #if it saves successfully
+      redirect_to markets_path
+    else #if it doesn't save
+      render :edit
+    end
+  end
+
+
+
+  private
+
+    def market_params
+      params.require(:market).permit(:name, :address, :city, :state, :county, :zip)
+    end
 end
