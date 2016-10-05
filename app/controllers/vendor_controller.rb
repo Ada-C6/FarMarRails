2be1daf20ec48c36d40db2f79b882544b8fb14e2
Nicole Url <nicole.url.4@gmail.com>
2016-10-05 15:39:33 -0700

    Added all standard views for vendor-related stuff.

--- app/controllers/vendor_controller.rb
+++ app/controllers/vendor_controller.rb
@@ -1,30 +1,39 @@
 class VendorController < ApplicationController
 
   def index
-
+    @vendors = Vendor.all
   end
 
   def create
+    @vendor = Vendor.create(vendor_params)
 
+    redirect_to root_path
   end
 
   def new
-
+    @vendor = Vendor.new
   end
 
   def edit
-
+    @vendor = Vendor.find(params[:id])
   end
 
   def show
-
+    @vendor = Vendor.find(params[:id])
   end
 
   def update
+    @vendor = Vendor.find(params[:id])
 
+    if @vendor.update(vendor_params)
+      redirect_to vendor_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @vendor = Vendor.find(params[:id]).destroy
   end
+
 end
