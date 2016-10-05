83f011bd091c61875552d8e4fa3ed8c0e016dc39
Nicole Url <nicole.url.4@gmail.com>
2016-10-05 14:01:36 -0700

    Market index page laid out in a table with actions on individual markets

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -4,27 +4,35 @@ class MarketController < ApplicationController
   end
 
   def create
+    @market = Market.create(market_params)
 
+    redirect_to root_path
   end
 
   def new
-
+    @market = Market.new
   end
 
   def edit
-
+    @market = Market.find(params[:id])
   end
 
   def show
-
+    @market = Market.find(params[:id])
   end
 
   def update
+    @market = Market.find(params[:id])
 
+    if @market.update(market_params)
+      redirect_to market_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @market = Market.find(params[:id]).destroy
   end
 
 end
