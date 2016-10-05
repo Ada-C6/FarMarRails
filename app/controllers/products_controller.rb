1643c4ca5f824b74ca2d2974d86afd93fb1b653f
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-05 15:59:24 -0700

    Create a Product is working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,25 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @vendor = Vendor.find(params[:vendor_id])
+    @product = Product.new
+  end
+
+  def create
+    @vendor = Vendor.find(params[:vendor_id])
+    @product = Product.new(product_params)
+    @product.vendor_id = params[:vendor_id]
+    if @product.save
+      redirect_to vendor_products_path
+    else
+      render :new
+    end
+  end
+end
+
+
+private
+def product_params
+  params.require(:product).permit(:name)
 end
