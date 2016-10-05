684251591bf6326271cad4b1547c47b057241eff
LaurenSky <ljherrer@gmail.com>
2016-10-04 17:00:54 -0700

    added delete function and update function for product by vendor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,16 +8,23 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def Update
+  def update
     @product = Product.find(params[:id])
 
-  if @product.update(task_params)
-    # saved successfully
-    redirect_to product_path
-  else
-    # did not save
-    render :edit
+    if @product.update(task_params)
+      # saved successfully
+      redirect_to vendor_products_path
+    else
+      # did not save
+      render :edit
+    end
   end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+    redirect_to vendor_products_path
   end
 
   private
