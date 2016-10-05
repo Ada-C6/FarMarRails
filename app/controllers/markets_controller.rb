6050342563d41dd55b8036e0326de52387ef21c1
Johna N. Morris <johna.n.morris@gmail.com>
2016-10-05 14:54:01 -0700

    Implemented new and create controller actions; implemented new view

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -3,12 +3,29 @@ class MarketsController < ApplicationController
     @markets = Market.all
   end
 
+  def new
+    @market = Market.new
+  end
+
+  def create
+    @market = Market.new(market_params)
+    if @market.save
+      redirect_to markets_path
+    else
+      render :new
+    end
+  end
 
 # TODO:
-# markets#create
-# markets#new
 # markets#edit
 # markets#show
 # markets#update
 
+# ---- PRIVATE METHODS ----
+
+  private
+
+  def market_params
+    params.require(:market).permit(:name, :address, :city, :county, :state, :zip)
+  end
 end
