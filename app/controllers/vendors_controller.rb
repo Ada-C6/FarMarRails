b05d58557ea37ea3bd7d06c8526cee0d8f0a00c8
Dianne Laguerta <diannelaguerta@gmail.com>
2016-10-04 17:02:04 -0700

    added the view for viewing all markets, WIP

---
+++ app/controllers/vendors_controller.rb
@@ -0,0 +1,3 @@
+class VendorsController < ApplicationController
+  def index; end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get '/' => 'markets#home'
+
+  get 'markets/all' => 'markets#all'
+
+  resources :markets, except: [:destroy]
+
+
+  get '/vendors' => 'vendors#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
