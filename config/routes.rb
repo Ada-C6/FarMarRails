78b16bd59bdde9f1b3fe020262e6f633ad07750a
heatherherrington <redherring@gmail.com>
2016-10-05 13:42:13 -0700

    Filled in what information needs to go on each page, minor formatting

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,6 @@ Rails.application.routes.draw do
     resources :sales, only: [:show, :create]
   end
 
-
-
-
-
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
