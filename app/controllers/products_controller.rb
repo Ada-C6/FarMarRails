0c29642f266cf7f78aa3ce39a096941d34586c32
heatherherrington <redherring@gmail.com>
2016-10-05 15:55:58 -0700

    WIP - Trying to edit/delete product in vendors/show.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,41 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to vendor_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id]).destroy
+    redirect_to vendor_path
+  end
+
+  def edit  # Will give the form, like new
+    @product = Product.find(params[:id])
+  end
+
+  def update # Actually do the update, like create
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to vendor_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
