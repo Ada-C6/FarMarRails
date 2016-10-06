59537eed0607d4202a999693dbc773f79c3be81e
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-06 10:22:47 -0700

    Built form & vendor actions to add new vendor from a market page.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -7,4 +7,26 @@ class VendorsController < ApplicationController
     @market = Market.find(@vendor.market_id)
     #TODO: If we want to restrict users from messing with our URLs - changing the market_id in the URL when they're looking at a vendor, we should do some checking here to make sure that params[:market_id] == @vendor.market_id
   end
+
+  def new
+    @market = Market.find(params[:market_id])
+    @vendor = @market.vendors.new
+  end
+
+  def create
+    @market = Market.find(params[:market_id])
+    @vendor = @market.vendors.new(vendor_params)
+
+    if @vendor.save
+      redirect_to market_path(@market)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:market_id, :name, :num_employees)
+  end
 end
