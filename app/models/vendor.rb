071b4a8909b1169a56ed4037e218b19557147430
Nicole Url <nicole.url.4@gmail.com>
2016-10-06 16:53:44 -0700

    We tried to do something in the vendor model, but decided to stop.

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,9 +3,4 @@ class Vendor < ActiveRecord::Base
   has_many :products
   has_many :sales
 
-  def add_vendor_link
-    if params[:market_id] != nil
-      return link_to "Add a new vendor for this market", new_market_vendor_path(@vendors[0].market_id)
-    end
-  end
 end
