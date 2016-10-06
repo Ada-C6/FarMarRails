821f21a4b8ffab6c5dc79d249618bc3bbf3e0463
heatherherrington <redherring@gmail.com>
2016-10-06 08:30:39 -0700

    Completed edit/create functionality for vendor/product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,25 +8,30 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
+    @vendor = Vendor.find(params[:vendor_id])
+    @product = @vendor.products.build
   end
 
   def create
     @product = Product.new(product_params)
+    @vendor = Vendor.find(params[:vendor_id])
+    @product.vendor_id = @vendor.id
     if @product.save
-      redirect_to vendor_path
+      redirect_to vendor_path(@vendor.id)
     else
       render :new
     end
   end
 
   def destroy
+    @vendor = Vendor.find(params[:vendor_id])
     @product = Product.find(params[:id]).destroy
-    redirect_to vendor_path
+    redirect_to vendor_path(@vendor.id)
   end
 
   def edit  # Will give the form, like new
     @product = Product.find(params[:id])
+    @vendor = Vendor.find(params[:vendor_id])
   end
 
   def update # Actually do the update, like create
@@ -41,6 +46,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, {:vendors => [:id, :market_id, :name, :employee_number]})
+    params.require(:product).permit(:name,
+      :vendors => [:name, :employee_number, :market_id])
   end
 end
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -8,4 +8,42 @@ class VendorsController < ApplicationController
     @vendor = Vendor.find(params[:id])
   end
 
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+    if @vendor.save
+      redirect_to market_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @vendor = Vendor.find(params[:id]).destroy
+    redirect_to market_path
+  end
+
+  def edit  # Will give the form, like new
+    @vendor = Vendor.find(params[:id])
+    @market = Market.find(params[:market_id])
+  end
+
+  def update # Actually do the update, like create
+    @vendor = Vendor.find(params[:id])
+    if @vendor.update(vendor_params)
+      redirect_to market_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :employee_number,
+      :markets => [:name, :address, :city, :county, :state, :zip])
+  end
 end
