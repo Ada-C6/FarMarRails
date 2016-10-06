9720ac7071f4aa6fd488401bdc58d9fea62af9ff
Jessica <jchung722@gmail.com>
2016-10-06 13:14:10 -0700

    added monthly sale list and total

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -20,6 +20,8 @@ class SalesController < ApplicationController
   def index
     @sales = Sale.where(vendor_id:params[:id])
     @sale_total = total(@sales).to_f/100
+    @month_sales = monthfind(@sales)
+    @month_sale_total = total(@month_sales).to_f/100
   end
 
   def total(sales)
@@ -28,5 +30,15 @@ class SalesController < ApplicationController
       total += sale.amount
     end
     return total
+  end
+
+  def monthfind(sales)
+    current_month_sales = []
+    sales.each do |sale|
+      if sale.purchase_time.month == Time.now.month
+        current_month_sales << sale
+      end
+    end
+    return current_month_sales
   end
 end
