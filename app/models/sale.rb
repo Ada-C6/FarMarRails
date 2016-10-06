65e7c05b4202ced3f2963e684a46c0bb92911b0e
laurenfb <laurenfriesbrink@gmail.com>
2016-10-06 14:42:34 -0700

    move sales logic to controller

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,23 @@
 class Sale < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :product
+
+  def self.total(sales)
+    total = 0
+    sales.each do |sale|
+      total += sale.amount
+    end
+    return total
+  end
+
+  def self.monthfind(sales)
+    current_month_sales = []
+    sales.each do |sale|
+      if sale.purchase_time.month == Time.now.month
+        current_month_sales << sale
+      end
+    end
+    return current_month_sales
+  end
+
 end
