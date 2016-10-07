74fe76825ac2ee421f153dfbba2ef877922faa2e
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-07 13:40:05 -0700

    sales_controller: added new and update methods. sale model: validated user input. sales view new: created file. CREATING a sale from the vendor_product page is functional.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,28 @@
 class SalesController < ApplicationController
+    def new
+        @sale = Sale.new
+        @vendor_id = params[:vendor_id]
+        @product_id = params[:product_id]
+    end
+
+    def create
+        @vendor_id = params[:vendor_id]
+        @product_id = params[:product_id]
+        @sale = Sale.new(product_params)
+        @sale[:vendor_id] = @vendor_id
+        @sale[:product_id] = @product_id
+        @sale[:purchase_time] = Time.now.utc
+        if @sale.save  # successful
+            redirect_to vendor_path(@sale.vendor)
+        else  # unsuccessful, return to form
+            render :new
+        end
+    end
+
+
+    private
+
+    def product_params
+        params.require(:sale).permit(:amount)
+    end
 end
