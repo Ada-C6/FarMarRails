d612c9709e19fc5e384658a8e6259d4210ce618c
Yuri Nakashima <yuri310@gmail.com>
2016-10-06 17:05:11 -0700

    working on products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,20 @@ class ProductsController < ApplicationController
     this_vendor = Integer(params[:id])
     @this_vendors_products = Product.where(vendor_id: this_vendor)
   end
+
+  def new; end
+
+  def create_product
+    this_vendor = Integer(params[:id])
+    @this_product = Product.new
+    @this_product.vendor_id = this_vendor.id
+    @this_product.save
+    if @this_product.save
+      redirect_to vendor_products_new_path
+    else
+      render :new_product
+    end
+  end
 end
 
 private
