8268bf5772981bdc5053543d43feb8629016be92
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-06 19:04:18 -0700

    products_controller and products edit view--working on editing and updating. This isn't working yet, my brain is done for the night.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,21 @@ class ProductsController < ApplicationController
     def edit
         @product = Product.find(params[:id])
     end
+
+    def update
+        @product = Product.find(params[:id])
+        @product[:vendor_id] = @product.vendor.id
+        if @product.update(product_params)
+            redirect_to vendor_path(@product.vendor)
+        else
+            render :edit
+        end
+    end
+
+
+    private
+
+    def product_params
+        params.require(:name, :vendor_id)
+    end
 end
