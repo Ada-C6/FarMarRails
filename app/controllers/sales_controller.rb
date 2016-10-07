a431a5696a9d1569d6bf03b1d590b8865cbc36b6
Madeleine Johnson <mjohnson3038@gmail.com>
2016-10-07 14:41:33 -0700

    Whoops, on the master

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -2,10 +2,15 @@ class SalesController < ApplicationController
   def index
     this_vendor = Integer(params[:id])
     @this_vendors_sales = Sale.where(vendor_id: this_vendor)
+
+
+    # @current_time = DateTime.now.strftime "%m/%y"
+    @start = Date.new(2013,11)
+    @current_month = @start..(@start + 30)
   end
 
   def new
-    this_vendor = Vendor.find(params[:id]).# IDEA:
-    
+    this_vendor = Vendor.find(params[:id])
+
   end
 end
