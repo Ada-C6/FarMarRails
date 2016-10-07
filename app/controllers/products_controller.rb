9897003d3be7f3c8e6be18d7fda05ab414915cb0
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-07 12:26:19 -0700

    products_controller: created new and update methods. products view new: created file. Adding a new product to a vendor works.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,26 @@
 class ProductsController < ApplicationController
+    def new
+        @product = Product.new
+        @vendor_id = params[:vendor_id]
+    end
+
+    def create
+        @vendor_id = params[:vendor_id]
+        @product = Product.new(product_params)
+        @product[:vendor_id] = @vendor_id
+        if @product.save  # successful
+            redirect_to vendor_path(@product.vendor)
+        else  # unsuccessful, return to form
+            render :new
+        end
+    end
+
     def edit
         @product = Product.find(params[:id])
     end
 
     def update
         @product = Product.find(params[:id])
-        # @product[:vendor_id] = @product.vendor.id
         if @product.update(product_params)
             redirect_to vendor_path(@product.vendor)
         else
@@ -13,10 +28,16 @@ class ProductsController < ApplicationController
         end
     end
 
+    # def destroy
+    #     @product = Product.find(params[:id])
+    #     @product.destroy
+    #     redirect_to vendor_path(@product.vendor)
+    # end
+
 
     private
 
     def product_params
-        params.require(:product).permit(:name, :vendor_id)
+        params.require(:product).permit(:name)
     end
 end
