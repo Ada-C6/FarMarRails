9a9ed6f6445a67e1231fec6bc279f1d46aa94398
Mary Morrison <marymorrison15@gmail.com>
2016-10-07 10:02:04 -0700

    as vendor, can see list of all sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,13 @@
 class SalesController < ApplicationController
+
+  def index
+    @sales = Sale.all
+  end
+
+  def show
+    @sale = Sale.find(params[:id])
+    @vendor= Vendor.find(@sale.vendor_id)
+
+  end
+
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,13 @@
 class Sale < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :product
-  
+
+
+
+  # def total_sales
+  #   total = []
+  #   @sale = Sale.find(params[:id])
+  #   total << @sale.amount
+  #   total.inject(0){|sum,x| sum + x }
+  # end
 end
