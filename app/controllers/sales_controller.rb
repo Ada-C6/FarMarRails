5931798f18cde133ac7fdabd312f0b09eb4541eb
Yuri Nakashima <yuri310@gmail.com>
2016-10-07 14:41:58 -0700

    completed create Sale for Product

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,7 +5,25 @@ class SalesController < ApplicationController
   end
 
   def new
-    this_vendor = Vendor.find(params[:id]).# IDEA:
-    
+    this_vendor = Vendor.find(params[:id]).id
+    this_product = Product.where(vendor_id: this_vendor)
+    @new_sale = Sale.new(vendor_id: this_vendor, product_id: this_product)
+  end
+
+  def create
+    this_vendor = Vendor.find(params[:id]).id
+    this_product = Product.where(vendor_id: this_vendor)
+    @new_sale = Sale.new(vendor_id: this_vendor, product_id: this_product)
+    @new_sale.save
+    if @new_sale.update(sale_params)
+      redirect_to vendor_sales_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def sale_params
+    params.require(:sale).permit(:amount, :time)
   end
 end
