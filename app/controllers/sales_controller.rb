fc9f8db9e8d5b8f6795698b07908c8500708ee8c
Karin Kubischta <karin.kubischta@gmail.com>
2016-10-06 17:17:12 -0700

    refactored total_sale to take argument, added current_sales variable

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,5 +3,7 @@ class SalesController < ApplicationController
     @vendor = Vendor.find(params[:vendor_id])
     @sales = @vendor.sales
 
+    @current_sales = @sales.where(purchase_time: (1.month.ago)...DateTime.now)
+    # current sales is supposed to be "this month", and what it's currently doing is the sales from a month ago up to now (so not restricted on THIS month)
   end
 end
--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -4,9 +4,10 @@ module SalesHelper
     product.name
   end
 
-  def total_sales
+# NOTE: total_sales might be better living somewhere else, perhaps the model?
+  def total_sales(sales)
     total = 0
-    @sales.each do |sale|
+    sales.each do |sale|
       total += sale.amount
     end
     return total
