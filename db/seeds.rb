aa7f9584cd70404d0652df06cf227052b81e19ef
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-07 15:41:41 -0700

    Added config changes for images on Heroku pt 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'CSV'
+require 'csv'
 
 markets_list = []
 CSV.read("./seed_csvs/markets.csv").each do |line|
