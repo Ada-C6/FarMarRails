65f19a25ef6420de4dcf2e38a3f7a6a5dbd1b2c1
Nicole Url <nicole.url.4@gmail.com>
2016-10-06 22:30:05 -0700

    Added a mixin for methods to determine which path we're on - vendor, product, or market

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
     resources :product, :sale
   end
 
-
+  # Getting to sales through product
+  resources :product, only: [:index, :new, :create] do
+    resources :sale
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
