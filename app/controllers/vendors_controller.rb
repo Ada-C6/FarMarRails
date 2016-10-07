d83786d6d8269d17cda6ece76f74e9a4e65b167f
Yasmin <yasminor@gmail.com>
2016-10-07 15:19:38 -0700

    Fixed relational links between edit and create vendor pages and the market show page

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -10,9 +10,40 @@ class VendorsController < ApplicationController
 
     def new
       @vendor = Vendor.new
+      @market_id = params[:market_id]
     end
 
     def edit
       @vendor = Vendor.find(params[:id])
     end
+
+    def create
+      @market_id = params[:market_id]
+      @vendor = Vendor.new(vendor_params)
+      @vendor[:market_id] = @market_id
+      if @vendor.save
+        redirect_to market_path(@market_id)
+      else
+        render :new
+      end
+    end
+
+    # def update
+    #
+    # end
+
+    def destroy
+      Vendor.find(params[:id]).destroy
+      redirect_to market_path
+    end
+
+    def confirm_deletion
+      @vendor = vendor.find(params[:id])
+    end
+
+  private
+    def vendor_params
+      params.require(:vendor).permit(:name, :num_employees)
+    end
+
 end
